ff6ee4ca502799f0dfc2827797d8d3cd4b48843e
ArthurCoutto <arthur.coutto@gmail.com>
2021-12-17 17:59:01 -0300

    Update App.js
    Imports React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import './index.css';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}from 'react-router-dom';
+
+
 
 const logotipo = <img src="../assets/Logo1.png" alt="logo"></img>
